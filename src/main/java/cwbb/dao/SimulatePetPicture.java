199eb95edfcb1e3964bf3ef50408c8f1b64fde88
zouwenchang <1399234704@qq.com>
2022-12-02 16:41:36 +0800

    一次修改

---
+++ src/main/java/cwbb/dao/SimulatePetPicture.java
@@ -0,0 +1,9 @@
+package cwbb.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface SimulatePetPicture extends BaseMapper<SimulatePetPicture> {
+}
